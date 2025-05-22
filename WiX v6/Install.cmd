0bd43da77432cfabad331bfca7bd662ac80d35a9
Mike Williams <mike.williams@tba.group>
2025-05-22 10:17:56 +0100

    Experiment to see if adding wxl file fixes rogue errors in logs

---
+++ WiX v6/Install.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+msiexec /i "bin\x86\Debug\en-us\WiX v6 Sample.msi" /l*v "install.log"
+
+pause
---
+++ WiX v6/UnInstall.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+msiexec /x "bin\x86\Debug\en-us\WiX v6 Sample.msi" /l*v "uninstall.log"
+
+pause
